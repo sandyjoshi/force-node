06d09ae5118bd1759d3e45cda5b49c795cf84166
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2015-10-16 13:52:23 +0530

    scaling the sprites

--- app/game.js
+++ app/game.js
@@ -11,29 +11,24 @@ function preload() {
     game.load.spritesheet('veggies', './assets/images/fruits_dummy.png', 32, 32);
 }
 
- var slashes , points = [];
+var slashes , points = [];
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     var background = game.add.sprite(0,0,'background');
     background.scale.y = 2;
-
     emitter = game.add.emitter(game.world.centerX, 0, 250);
+    emitter.minParticleScale = 3;
     emitter.makeParticles('veggies', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 200, true, true);
     emitter.minParticleSpeed.setTo(-200, -300);
     emitter.maxParticleSpeed.setTo(200, -400);
     emitter.gravity = 150;
     emitter.bounce.setTo(0.5, 0.5);
-    emitter.angularDrag = 30;
+    emitter.angularDrag = 50;
     emitter.start(false, 8000, 400);
-
 	slashes = game.add.graphics(0, 0);
-
-
 }
-
 function update() {
-
 	points.push({
 		x: game.input.x,
 		y: game.input.y
@@ -45,10 +40,9 @@ function update() {
 	slashes.alpha = .5;
 	slashes.moveTo(points[0].x, points[0].y);
 	for (var i=1; i<points.length; i++) {
-	slashes.lineTo(points[i].x, points[i].y);
+	   slashes.lineTo(points[i].x, points[i].y);
 	}
 	slashes.endFill();
-
 }
 
 function render() {
