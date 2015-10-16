45fcc51bd6c71d52de20e55391920e28a2e811ab
Sandeep Joshi <sandeepjoshi@Sandeeps-MacBook-Pro.local>
2015-10-16 12:57:22 +0530

    add splash movement

--- app/game.js
+++ app/game.js
@@ -7,8 +7,10 @@ var game = new Phaser.Game(width, height, Phaser.AUTO, 'game-container', {
 });
 
 function preload() {
-    game.load.spritesheet('veggies', '../assets/images/fruits_dummy.png', 32, 32);
+    game.load.spritesheet('veggies', './assets/images/fruits_dummy.png', 32, 32);
 }
+
+ var slashes , points = [];
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -20,9 +22,29 @@ function create() {
     emitter.bounce.setTo(0.5, 0.5);
     emitter.angularDrag = 30;
     emitter.start(false, 8000, 400);
+
+	slashes = game.add.graphics(0, 0);
+
+
 }
 
 function update() {
+
+	points.push({
+		x: game.input.x,
+		y: game.input.y
+	});
+	points = points.splice(points.length-10, points.length);
+
+	slashes.clear();
+	slashes.beginFill(0xFFFFFF);
+	slashes.alpha = .5;
+	slashes.moveTo(points[0].x, points[0].y);
+	for (var i=1; i<points.length; i++) {
+	slashes.lineTo(points[i].x, points[i].y);
+	}
+	slashes.endFill();
+
 }
 
 function render() {
